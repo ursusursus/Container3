d6af8af4642b4b8afd88d612d4c98985c312f883
Vlastimil Brečka <vlastimil@vlastimilbrecka.sk>
2017-12-08 20:35:52 +0100

    pridany submodul

--- app/src/main/java/sk/ursus/container3/MainActivity.java
+++ app/src/main/java/sk/ursus/container3/MainActivity.java
@@ -3,11 +3,14 @@ package sk.ursus.container3;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import sk.ursus.library.Foo;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Foo.bar();
         setContentView(R.layout.activity_main);
     }
 }
